748192088ce970a687187b3baec29be2bf7bdb2b
Chris Gabor <chrisgabor8@gmail.com>
2019-12-02 21:14:50 -0800

    dataset builder

---
+++ buildDataset.py
@@ -0,0 +1,87 @@
+import tarfile
+# import tensorflow as tf
+# print( tf.constant('Hello from TensorFlow ' + tf.__version__) )
+import pathlib
+import os
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import time
+import random
+import shutil
+import json
+
+
+def get_dir_mapping(path):
+    dirs = sorted([name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))])
+    num_classes = len(dirs)
+    dir_map = {}
+    for i in range(0, num_classes):
+        images_path = os.path.join(path, dirs[i])
+        dir_map[i] = [os.path.join(dirs[i], x) for x in os.listdir(images_path)]
+    return dir_map
+
+
+def write_json_dict(path, fname, dict_map):
+    json_dict = json.dumps(dict_map)
+    class_mapping_path = os.path.join(path, fname)
+    with open(class_mapping_path, 'w') as f:
+        f.write(json_dict)
+
+
+def get_class_counts(dir_map):
+    counts = {}
+    for k, v in dir_map.items():
+        counts[int(k)] = len(v)
+    return counts
+
+
+class DatasetBuilder:
+
+    def __init__(self, dataset_dir="C:/Users/Home/.keras/datasets/", base_dir="CUB_200_2011"):
+        self.dataset_dir = dataset_dir
+        self.base_dir = os.path.join(self.dataset_dir, base_dir)
+        self.image_dir = os.path.join(self.base_dir, 'images')
+
+    def get_base_class_mapping(self):
+        class_path = os.path.join(self.base_dir, 'classes.txt')
+        with open(class_path) as file:
+            classes = dict((line.strip().split(' ') for line in file))
+        classes = {(int(k)-1):v for k,v in classes.items()}
+        return classes
+
+    def random_downsample(self, num_classes, ratio):
+        dir_map = get_dir_mapping(self.image_dir)
+        new_set = {}
+        targets = random.sample(list(dir_map), num_classes)
+        for k, v in dir_map.items():
+            if k in targets:
+                count = len(v)
+                sample_size = math.ceil(ratio*count)
+                v = random.sample(v, sample_size)
+            new_set[k] = v
+        return new_set
+
+    def create_new_dataset(self, target, num_downsample=200, ratio=0.05):
+        new_data_path = os.path.join(self.dataset_dir, target)
+        new_image_path = os.path.join(new_data_path, 'images')
+        new_dataset = self.random_downsample(num_downsample, ratio)
+        for k, v in new_dataset.items():
+            print("Creating dir: ", k)
+            for path in v:
+                source = os.path.join(self.image_dir, path)
+                dest = os.path.join(new_image_path, path)
+                print("Copying ", source, end=' ')
+                print("to ", dest)
+                os.makedirs(os.path.dirname(dest), exist_ok=True)
+                shutil.copyfile(source, dest)
+
+        write_json_dict(new_data_path, 'class_mapping.json', self.get_base_class_mapping())
+        write_json_dict(new_data_path, 'class_counts.json', get_class_counts(get_dir_mapping(new_image_path)))
+
+
+if __name__ == "__main__":
+    builder = DatasetBuilder()
+    # dir_map = builder.random_downsample(140, 0.1)
+    builder.create_new_dataset('full_data',0, 1)
+
--- load_birds.py
+++ load_birds.py
@@ -1,20 +1,84 @@
 import tarfile
-import tensorflow as tf
+# import tensorflow as tf
+# print( tf.constant('Hello from TensorFlow ' + tf.__version__) )
 import pathlib
+import os
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import time
+import random
+import shutil
 
-data_dir = tf.keras.utils.get_file(origin='http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz',
-                                   fname='bird_photos', untar=True)
-data_dir = pathlib.Path(data_dir)
+
+def download_data(origin='http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz'):
+    data_dir = tf.keras.utils.get_file(origin=origin, fname='bird_photos', untar=True, extract=True)
+    return data_dir
 
 
+class DataHandler:
+
+    def __init__(self, data_path = "C:/Users/Home/.keras/datasets", dataset='CUB_200_2011', target_size=(28, 28), batch_size=32):
+        self.data_dir = data_path
+        self.dataset_path = os.path.join(pathlib.Path(self.data_dir).parents[0], dataset)
+        self.image_data = os.path.join(self.dataset_path, 'images')
+        self.target_size = target_size
+        self.batch_size = batch_size
+        self.flow_gen = self._set_flow_generator()
+
+    def _set_flow_generator(self):
+        image_gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.)
+        image_gen = image_gen.flow_from_directory(self.image_data, target_size=self.target_size,
+                                                  batch_size=self.batch_size, class_mode='sparse')
+        return image_gen
+
+    def get_flow_generator(self):
+        return self.flow_gen
+
+    def get_class_mapping(self):
+        pass
 
 
-def unzip_data(path):
-    with tarfile.open(path, "r:gz") as so:
-        so.extractall(path="./data")
-    print('hi')
+### define model
+# load pretrained model
+# run images through pretrained model without new head
+# create new head
+
+## no active learning
+# train model on full data
+# train model on splits with weighted gradients
+
+### show confusion matrix
+# show per class precision, loss, accuracy,
+# show confusion matrix
+
+## entropy active learning
+
+## gradient length active learning
+
 
 
 if __name__ == "__main__":
-    # unzip_data("./data/CUB_200_2011.tgz")
-    pass
+    builder = DatasetBuilder()
+    builder.create_new_dataset("downsample")
+    # print(builder.get_base_class_counts())
+    # builder.random_downsample(10)
+
+    # dh = DataHandler(batch_size=256)
+    # image_generator = dh.get_flow_generator()
+    # class_names = dh.get_class_mapping()
+    # print(class_names)
+    # print(dh.get_path_map())
+    # for batch in image_generator:
+    #     image, labels = batch
+    #     break
+    #
+    # for i in range(9):
+    #     plt.subplot(3, 3, i+1)
+    #     plt.imshow(image[i])
+    #     plt.xlabel(class_names[labels[i]])
+    # plt.tight_layout()
+    # plt.show()
+    # plt.close()
+
+
